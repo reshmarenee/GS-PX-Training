3bd36cd3c9738f67f7faaf8886158d616ecb607f
Reshma <46741461+reshmarenee@users.noreply.github.com>
2023-05-08 13:29:19 +0530

    Installed PX Tag

--- pxweb.js
+++ pxweb.js
@@ -1,3 +1,10 @@
+(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-AQ9DFAUZEKGT-2");
+
+
 function allowlogin(usermail)
 {
     var a = document.getElementById("usermail").value;
